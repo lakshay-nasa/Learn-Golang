c9b6ddea4d897466a8d85abbed0528834c239220
lakshaynasa <lakshay@1717@gmail.com>
2022-09-19 23:26:46 +0530

    Go Data Types

---
+++ DataTypes/Integer.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// Signed Integers
+	var score int = 500
+	var balance int = -100
+
+	fmt.Println(score)
+	fmt.Println(balance)
+}
---
+++ DataTypes/Strings.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name = "user1"
+	var courseName string = "Golang"
+	var salary string
+	topicName := "strings"
+
+	fmt.Println(name)
+	fmt.Println(courseName)
+	fmt.Println(topicName)
+	fmt.Println(salary)
+}
