d486ea22e33ac8ba38fb0820c3e7aed6341f9c1e
lakshaynasa <lakshay1717@gmail.com>
2022-09-25 23:32:25 +0530

    Go Loop

--- Loops/for.go
+++ Loops/for.go
@@ -13,4 +13,27 @@ func main() {
 	for i := 0; i < 10; i++ {
 		fmt.Println(i)
 	}
+
+	var name = []string{"pop", "winny", "stranger", "yin", "luke", "tian"}
+
+	// The Continue Statement
+	fmt.Println("\nThe Continue Statement")
+	for i := 0; i < len(name); i++ {
+		if name[i] == "stranger" {
+			continue
+		}
+
+		fmt.Printf("Welocme! %v \n", name[i])
+	}
+
+	// The Break Statement
+	fmt.Println("\nThe Break Statement")
+	for i := 0; i < len(name); i++ {
+		if name[i] == "stranger" {
+			fmt.Println("Warning!!")
+			break
+		}
+
+		fmt.Printf("Welocme! %v \n", name[i])
+	}
 }
